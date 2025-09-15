c8fa8e8e40bf69c555f39bbc68102ec87685e24a
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-15 15:26:34 -0300

    Agregar logica crear temas

--- src/repos/temasRepo.js
+++ src/repos/temasRepo.js
@@ -8,6 +8,27 @@ function listWithVoteCountOrdenado(){
     return row
 }
 
+function create({titulo, descripcion}){
+    const descripcionLimpia = descripcion.trim();
+    const descripcion = descripcionLimpia || "";
+
+    if (titulo == '' || titulo.length > 120){
+        return {error:"Titulo demasiado largo"}
+    };
+
+    const nuevoTema = db.prepare(`
+    INSERT INTO temas (titulo, descripcion)
+    VALUES (?, ?);
+    `);
+    const id_nuevo = nuevoTema.lastInsertRowid;
+    const temaCreado = obtenerTema(id_nuevo);
+    return temaCreado;
+}
+
+function obtenerTema(id){
+    return db.prepare(`SELECT * FROM temas WHERE id = ?`).get(Number(id))
+}
+
 const result = listWithVoteCountOrdenado();
 console.log(result);
 module.exports = { listWithVoteCountOrdenado };
