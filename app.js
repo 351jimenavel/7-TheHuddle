7e019f6614f822fbce92816d2012f60d7dd9ede7
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-15 09:05:21 -0300

    Modificar acceso de ruta del router

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path');
 const router = require('./src/routes/temas');
-const temasRouter = require('./routes/temas');
+const temasRouter = require('./src/routes/temas');
 const app = express()
 const port = 3000;
 
