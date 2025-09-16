30af22b280d3151d4e550c5127a22f2b4bef7c49
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-15 22:38:22 -0300

    Agregar handler a rutas

--- src/routes/temas.js
+++ src/routes/temas.js
@@ -3,18 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 // Importar controlador
-const { getListaTemas } = require('../controllers/temasController');
+const { getListaTemas, getFormNuevoTema, postCrearTema } = require('../controllers/temasController');
 
 // Rutas de TEMAS
 router.get('/temas', getListaTemas);
 
 // Ruta de TEMAS/NUEVO
 // GET  /temas/nuevo     → muestra formulario de creación
-router.get('/temas/nuevo');
+router.get('/temas/nuevo', getFormNuevoTema);
 
 // Ruta TEMAS para procesa creación (validación + guardar + feedback)
 // POST /temas
-router.post('/temas');
+router.post('/temas', postCrearTema);
 
 // exportar router
 module.exports = router;
