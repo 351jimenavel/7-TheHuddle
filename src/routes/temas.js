965b87f45816c06a769583b48aa3291c6d5b5e3b
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-15 23:21:32 -0300

    Agregar rutas Update y Delete

--- src/routes/temas.js
+++ src/routes/temas.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Importar controlador
-const { getListaTemas, getFormNuevoTema, postCrearTema } = require('../controllers/temasController');
+const { getListaTemas, getFormNuevoTema, postCrearTema, getFormEditarTema, postEditarTema, postEliminarTema } = require('../controllers/temasController');
 
 // Rutas de TEMAS
 router.get('/temas', getListaTemas);
@@ -15,6 +15,13 @@ router.get('/temas/nuevo', getFormNuevoTema);
 // Ruta TEMAS para procesa creación (validación + guardar + feedback)
 // POST /temas
 router.post('/temas', postCrearTema);
+
+// Ruta UPDATE temas
+router.get('/temas/:id/editar', getFormEditarTema);
+router.post('/temas/:id/editar', postEditarTema);
+
+// Ruta DELETE temas
+router.post('/temas/:id/eliminar', postEliminarTema);
 
 // exportar router
 module.exports = router;
