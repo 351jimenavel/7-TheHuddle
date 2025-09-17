a034131718b9d03206ab0cee9532f0aaf4a81e38
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-16 23:32:59 -0300

    Fix en id, usar los del parametro

--- src/controllers/enlacesController.js
+++ src/controllers/enlacesController.js
@@ -9,19 +9,19 @@ function postCrearEnlace(req, res){
     const urlTrim = url.trim();
     const descripcionTrim = descripcion.trim();
 
-    if (tituloTrim === ""){
-        return res.redirect('/temas/:id?error=Datos invalidos');
+    if (tituloTrim === "" || urlTrim === ""){
+        return res.redirect('/temas/${temaId}?error=Datos invalidos');
     }
 
     try{
         enlacesRepo.create({tema_id: temaId, titulo: tituloTrim, url: urlTrim, descripcion:descripcionTrim});
-        return res.redirect('/temas/:id?ok=Enlace creado');
+        return res.redirect('/temas/${temaId}?ok=Enlace creado');
     }catch(err){
         if (err.message === 'CONFLICT'){
-            return res.redirect('/temas/:id?error=Ya existe un enlace con esa URL');
+            return res.redirect('/temas/${temaId}?error=Ya existe un enlace con esa URL');
         }
         console.error("postCrearEnlace error:", err);
-        return res.redirect('/temas/:id?error=No se pudo crear el enlace');
+        return res.redirect('/temas/${temaId}?error=No se pudo crear el enlace');
     }
 }
 
