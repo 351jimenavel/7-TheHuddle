435f29906f691b2157d57d5f031537c2fa916d0c
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-16 21:51:06 -0300

    Agregar nuevo router para editar/eliminar enlace

---
+++ src/routes/enlaces.js
@@ -0,0 +1,17 @@
+// enlaces.js (router)
+// Nuevo router para editar/eliminar enlace
+
+const express = require('express');
+const router = express.Router();
+
+const { getFormEditarEnlace, 
+    postEditarEnlace, 
+    postEliminarEnlace 
+    } = require('../controllers/enlacesController');
+
+
+router.get('/enlaces/:id/editar', getFormEditarEnlace);
+
+router.post('/enlaces/:id/editar', postEditarEnlace);
+
+router.post('/enlaces/:id/eliminar', postEliminarEnlace);
