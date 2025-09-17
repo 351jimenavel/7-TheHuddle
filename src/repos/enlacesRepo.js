e6f9c8ee2b5f52187de20582059068212890e6ee
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-16 23:12:53 -0300

    fix Pasar parametro temaId

--- src/repos/enlacesRepo.js
+++ src/repos/enlacesRepo.js
@@ -3,7 +3,7 @@ const db = require("../db.js");
 function listByTema(temaId){
     return db.prepare(`SELECT * FROM enlaces 
         WHERE tema_id = ? 
-        ORDER BY created_at DESC`).all();
+        ORDER BY created_at DESC`).all(Number(temaId));
 }
 
 function getById(id){
