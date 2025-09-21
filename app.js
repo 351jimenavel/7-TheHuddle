a9eda224a15ca9456711b7b46f32acc998a4444e
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-09-21 17:29:29 -0300

    Agregar comentarios guia

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-// Configurar Express, el motor EJS, la carpeta views y /public.
+// Configuracion basica
 const express = require('express')
 const path = require('path');
 
+// Routers de temas y enlaces
 const temasRouter = require('./src/routes/temas');
 const enlacesRouter = require('./src/routes/enlaces');
 
+// Inicializar app
 const app = express()
 const port = 3000;
 
+// Configuracion de motor de vistas y carpetas
 app.set('view engine', 'ejs'); // set the view engine to ejs
 app.set('views', path.join(__dirname, 'views'));// Indica a Express dónde encontrar los archivos de plantilla
 app.use(express.static(path.join(__dirname, 'public'))); // Sirve archivos estáticos desde la carpeta 'public'
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));    // middleware, permite leer datos de formularios enviados por POST.
 
+// Montar routers
 app.use(temasRouter);
 app.use(enlacesRouter);
 
+// Levantar servidor
 app.listen(port, () => {
     console.log("Servidor corriendo en puerto", `http://localhost:${port}/temas`);
 });
